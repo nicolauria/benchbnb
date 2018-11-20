86df1bfaa6820bff22012c6c6fd9a9557984e4cc
nico <nicolauria@outlook.com>
2018-11-20 12:45:30 -0800

    finish greeting container and component

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
 
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Bench BNB</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 })
