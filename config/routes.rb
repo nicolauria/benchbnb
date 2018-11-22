d593ce3d2d04389c625d6177183f6e86bf723567
nico <nicolauria@outlook.com>
2018-11-22 13:51:26 -0800

    bench bnb round 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  root 'static_pages#root'
-
-  namespace :api, defaults: { format: :json } do
-    resource :user, only: :create
-    resource :session, only: [:create, :destroy]
-  end
+  
 end
