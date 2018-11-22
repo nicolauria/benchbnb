167c6f427d3045b86d576357a5ed1bb1868469e1
nico <nicolauria@outlook.com>
2018-11-22 14:36:58 -0800

    finish user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   root 'static_pages#root'
+  namespace :api, default: { format: :json } do
+    resource :user, only: :create
+    resource :session, only: [:create, :destroy]
+  end
 end
