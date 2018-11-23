e30960861715ad4013ce4e7cf98a5f57ec4a4236
nico <nicolauria@outlook.com>
2018-11-23 12:26:19 -0800

    finish greeting container

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  
+
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Bench BNB</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
